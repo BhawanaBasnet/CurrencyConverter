a90dcab50edab59caa8a4ab205e5bec99c36ab7b
Bhawana Basnet <bhawanabasnet590@gmail.com>
2023-12-17 19:14:01 +0545

    Currency Calculator

---
+++ CurrencyConverter/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace CurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBoxToCurrency = new System.Windows.Forms.ComboBox();
+            this.comboBoxFromCurrency = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MintCream;
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.textBoxAmount);
+            this.panel1.Controls.Add(this.convertButton);
+            this.panel1.Controls.Add(this.textBoxResult);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.comboBoxToCurrency);
+            this.panel1.Controls.Add(this.comboBoxFromCurrency);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(74, 103);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(502, 476);
+            this.panel1.TabIndex = 0;
+            // 
+            // comboBoxToCurrency
+            // 
+            this.comboBoxToCurrency.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxToCurrency.FormattingEnabled = true;
+            this.comboBoxToCurrency.Location = new System.Drawing.Point(99, 271);
+            this.comboBoxToCurrency.Name = "comboBoxToCurrency";
+            this.comboBoxToCurrency.Size = new System.Drawing.Size(368, 30);
+            this.comboBoxToCurrency.TabIndex = 7;
+            // 
+            // comboBoxFromCurrency
+            // 
+            this.comboBoxFromCurrency.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxFromCurrency.FormattingEnabled = true;
+            this.comboBoxFromCurrency.Location = new System.Drawing.Point(99, 200);
+            this.comboBoxFromCurrency.Name = "comboBoxFromCurrency";
+            this.comboBoxFromCurrency.Size = new System.Drawing.Size(368, 30);
+            this.comboBoxFromCurrency.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(4, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(3, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "From:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(113, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(294, 26);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Nepali Currency Converter";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxResult.Location = new System.Drawing.Point(99, 328);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(368, 37);
+            this.textBoxResult.TabIndex = 9;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.convertButton.Location = new System.Drawing.Point(163, 393);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(160, 48);
+            this.convertButton.TabIndex = 10;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAmount.Location = new System.Drawing.Point(99, 130);
+            this.textBoxAmount.Multiline = true;
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(368, 38);
+            this.textBoxAmount.TabIndex = 11;
+            // 
+            // panel4
+            // 
+            this.panel4.AutoSize = true;
+            this.panel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel4.Location = new System.Drawing.Point(0, 130);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(93, 25);
+            this.panel4.TabIndex = 12;
+            this.panel4.Text = "Amount:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(658, 647);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxToCurrency;
+        private System.Windows.Forms.ComboBox comboBoxFromCurrency;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label panel4;
+        private System.Windows.Forms.TextBox textBoxAmount;
+    }
+}
+
---
+++ CurrencyConverter/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Populate ComboBoxes with currencies
+            comboBoxFromCurrency.Items.AddRange(new string[] { "USD", "EUR", "GBP" });
+            comboBoxToCurrency.Items.Add("NPR"); // Nepali Rupee
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            decimal amount;
+
+            if (decimal.TryParse(textBoxAmount.Text, out amount))
+            {
+                decimal convertedAmount = ConvertToNPR(amount, comboBoxFromCurrency.SelectedItem.ToString());
+                textBoxResult.Text = convertedAmount.ToString("0.00");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid amount.");
+            }
+        }
+        private decimal ConvertToNPR(decimal amount, string fromCurrency)
+        {
+            // Hardcoded conversion rates for demonstration
+            decimal usdToNPRRate = 133.47m;
+            decimal eurToNPRRate = 144.01m;
+            decimal gbpToNPRRate = 156.80m;
+
+            // Convert the amount to Nepali Rupee (NPR)
+            switch (fromCurrency)
+            {
+                case "USD":
+                    return amount * usdToNPRRate;
+                case "EUR":
+                    return amount * eurToNPRRate;
+                case "GBP":
+                    return amount * gbpToNPRRate;
+                default:
+                    return 0m;
+            }
+        }
+    }
+ 
+}
+
